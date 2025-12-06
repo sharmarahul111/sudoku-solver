115767592dbc195f7f8cc93f6d2087ed68a1899a
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2025-12-06 21:52:24 +0545

    Added getter functions for row, col, subgrid

---
+++ algorithm.js
@@ -0,0 +1,74 @@
+// import data from './data.json' with { type: 'json' };
+// console.log(data)
+const data = JSON.parse(`{"newboard":{"grids":[{"value":[[5,0,0,0,0,0,0,0,9],[0,0,0,0,0,0,0,7,0],[2,7,0,3,0,6,4,0,0],[0,4,0,0,0,0,9,0,0],[0,6,0,0,0,9,0,0,0],[0,0,5,1,0,0,0,0,4],[0,0,0,0,0,0,0,0,7],[0,0,7,0,3,0,0,0,0],[0,0,0,0,0,0,0,3,0]],"solution":[[5,1,4,2,8,7,3,6,9],[6,3,9,4,1,5,2,7,8],[2,7,8,3,9,6,4,1,5],[3,4,1,7,2,8,9,5,6],[7,6,2,5,4,9,1,8,3],[8,9,5,1,6,3,7,2,4],[1,8,3,9,5,2,6,4,7],[4,5,7,6,3,1,8,9,2],[9,2,6,8,7,4,5,3,1]],"difficulty":"Hard"}],"results":1,"message":"All Ok"}}`)
+const grid = data.newboard.grids[0].value
+
+class Cell {
+  constructor(grid, x, y) {
+    this.grid = grid
+    this.x = x
+    this.y = y
+    this.value = this.grid[x][y] // 0-9
+    this.possibleValues = []
+    this.type = type(this.value) // int | array | null
+  }
+  get row() {
+    let arr = []
+    this.grid[this.x].forEach((elm, i) => {
+      if (elm !== 0 && i != this.y) arr.push(elm)
+    });
+    return arr
+  }
+  get col() {
+    let arr = []
+    this.grid.forEach((row, j) => {
+      if (row[this.y] !== 0 && j != this.x) arr.push(row[this.y])
+    })
+    return arr
+  }
+  get subgrid() {
+    let startx = this.x - this.x % 3
+    let starty = this.y - this.y % 3
+    let arr = []
+    for (let i = startx; i < 3 + startx; i++) {
+      for (let j = starty; j < 3 + starty; j++) {
+        if (this.grid[i][j] !== 0 && !(i == this.x && j == this.y)) arr.push(this.grid[i][j])
+      }
+    }
+    return arr
+  }
+}
+
+
+
+let result = []
+for (let i = 0; i < grid.length; i++) {
+  result[i] = []
+  for (let j = 0; j < grid[i].length; j++) {
+    result[i].push(new Cell(grid, i, j))
+  }
+}
+
+// implementing first algorithm
+// filtering out only empty cells
+let temp = loop2d(result, (cell) => {
+  if (cell.value == 0) return cell
+})
+console.log(temp)
+// helper functions
+function type(data) {
+  if (Array.isArray(data)) return "array"
+  else if (Number.isInteger(data)) return "int"
+  else return "null"
+}
+// 2d foreach loop
+function loop2d(elm, func) {
+  let arr = []
+  elm.forEach((row, i) => {
+    row.forEach((col, j) => {
+      let r = func(col, i, j, row)
+      if (r) arr.push(r)
+    })
+  })
+  return arr
+}
