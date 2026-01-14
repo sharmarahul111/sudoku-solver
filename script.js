c956e70305c7240be619a7f15dbd0134ebe21484
SharmaRahul111 <sharmarahul111.psr@gmail.com>
2026-01-14 22:20:47 +0545

    Dynamically generate row/col boxes

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const section = document.querySelector("section.main")
-
+let text = "";
+for (let i = 0; i < 9; i++) {
+  text += `<div class="row">`
+  for (let j = 0; j < 9; j++) {
+    text += `<div class="col"></div>`
+  }
+  text += `</div>`
+}
+section.innerHTML = text
 function setValue(x, y, val) {
   // console.log(x,y,val, section.que)
   section.querySelectorAll("div.row")[x].
