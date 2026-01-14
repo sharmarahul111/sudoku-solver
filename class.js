ae219c6dc9677225495094a1d1289b99b9407b0f
SharmaRahul111 <sharmarahul111.psr@gmail.com>
2026-01-14 22:48:44 +0545

    Code refactor, utilizing Set() datatype in javascript

--- class.js
+++ class.js
@@ -1,26 +1,29 @@
 class Cell {
+  static values = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9])
   constructor(grid, x, y) {
     this.grid = grid
     this.x = x
     this.y = y
     this.value = this.grid[x][y] // 0-9
-    this.possibleValues = []
-    this.type = type(this.value) // int | array | null
+    this.possibleValues = new Set()
   }
+  // get set of values in corresponding row
   get row() {
     let arr = []
     this.grid[this.x].forEach((elm, i) => {
       if (elm !== 0 && i != this.y) arr.push(elm)
     });
-    return arr
+    return new Set(arr)
   }
+  // get set of values in corresponding column
   get col() {
     let arr = []
     this.grid.forEach((row, j) => {
       if (row[this.y] !== 0 && j != this.x) arr.push(row[this.y])
     })
-    return arr
+    return new Set(arr)
   }
+  // get set of values in corresponding subgrid
   get subgrid() {
     let startx = this.x - this.x % 3
     let starty = this.y - this.y % 3
@@ -30,6 +33,6 @@ class Cell {
         if (this.grid[i][j] !== 0 && !(i == this.x && j == this.y)) arr.push(this.grid[i][j])
       }
     }
-    return arr
+    return new Set(arr)
   }
 }
--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// import data from './data.json' with { type: 'json' };
+// console.log(data)
+const data = JSON.parse(`{"newboard":{"grids":[{"value":[[5,0,0,0,0,0,0,0,9],[0,0,0,0,0,0,0,7,0],[2,7,0,3,0,6,4,0,0],[0,4,0,0,0,0,9,0,0],[0,6,0,0,0,9,0,0,0],[0,0,5,1,0,0,0,0,4],[0,0,0,0,0,0,0,0,7],[0,0,7,0,3,0,0,0,0],[0,0,0,0,0,0,0,3,0]],"solution":[[5,1,4,2,8,7,3,6,9],[6,3,9,4,1,5,2,7,8],[2,7,8,3,9,6,4,1,5],[3,4,1,7,2,8,9,5,6],[7,6,2,5,4,9,1,8,3],[8,9,5,1,6,3,7,2,4],[1,8,3,9,5,2,6,4,7],[4,5,7,6,3,1,8,9,2],[9,2,6,8,7,4,5,3,1]],"difficulty":"Hard"}],"results":1,"message":"All Ok"}}`)
+const grid = data.newboard.grids[0].value
+// initializing the data
+let result = []
+for (let i = 0; i < grid.length; i++) {
+  result[i] = []
+  for (let j = 0; j < grid[i].length; j++) {
+    result[i].push(new Cell(grid, i, j))
+  }
+}
+
+
+// dynamically add row/comumn div to html file
 const section = document.querySelector("section.main")
 let text = "";
 for (let i = 0; i < 9; i++) {
@@ -8,13 +23,14 @@ for (let i = 0; i < 9; i++) {
   text += `</div>`
 }
 section.innerHTML = text
+
+// Helper function to set numeric value give x,y coordinates
 function setValue(x, y, val) {
-  // console.log(x,y,val, section.que)
   section.querySelectorAll("div.row")[x].
     querySelectorAll("div.col")[y].innerHTML = val
 
 }
-
+// Set the value of whole grid all at once
 function setGrid(data) {
   data.forEach((row, i) => {
     row.forEach((col, j) => {
